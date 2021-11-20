b014bff759093f16d2375049366d2adcc2b69963
Sergi <sergi.hurtado@basetis.com>
2021-11-20 12:56:22 +0100

    Init xCode Project

---
+++ Bitpanda/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitpanda
+//
+//  Created by Sergi Hurtado on 20/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
